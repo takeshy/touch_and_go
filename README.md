0feaf71f0a5b56a8c713fc9e98e017acc2065ebc
Takeshi Morita <takesy.morito@gmail.com>
2018-11-02 06:57:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# touch_and_go - File watcher
+
+## Abstract
+
+touch_and_go is a file watcher written in Golang. touch_and_go watches files and touch when one of these files is created, updated or removed.
+I use https://github.com/yosssi/goat as reference. thanks yosssi.
+
+## Use cases
+
+You can use touch_and_go to Docker for Windows on shared directory.
+because currently, inotify does not work on Docker for Windows. 
+execute touch_and_go on docker container and if file changed, touch_and_go execute touch command that file for iNotify. 
+So App as Rails, webpack-dev-server etc. can detect file changes.
+
+## Installation
+
+### Binary for Linux X64
+
+https://s3-ap-northeast-1.amazonaws.com/takesy-work/touch_and_go
+
+### From source codes
+
+```sh
+$ go get github.com/takeshy/touch_and_go
+```
+```sh
+$ wget https://raw.githubusercontent.com/takeshy/touch_and_go/master/main.go
+$ go build
+```
+
+## Configuration file
+
+The JSON file looks like the following:
+
+```json
+{
+  "watchers": [
+    {
+      "directory": "/home/ubuntu/my_project",
+      "excludes": ["node_modules"],
+    }
+  ]
+}
+```
+
+* `watchers` defines an array of file watchers. Each watcher definition has the following properties:
+  * `excludes` (optional)
+  * `directory` (optional)
+* `excludes` defines an array of file names which is out of watching range.
+* `directory` defines absolute directory path or the subdirectory. touch_and_go watches all files in and under this directory.  Defaults to current directory, if not specified.
+
+## Execution
+
+On the your project root directory execute the following command:
+
+```sh
+$ touch_and_go -c config.json -i 3000 &
+```
+-i An interval(ms) of a watchers default: 3000
+-c config file default: config.json
---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type FileStatus struct {
+	LastModTime int64
+	ModTime     int64
+}
+type Watcher struct {
+	Directory string   `json:"directory"`
+	Excludes  []string `json:"excludes"`
+	JobsC     chan<- Job
+	Targets   map[string]map[string]FileStatus
+}
+
+type Config struct {
+	Watchers []*Watcher `json:"watchers"`
+}
+
+type Job struct {
+	Kind string
+	Path string
+}
+
+func main() {
+	configFile := flag.String("c", "config.json", "Config File")
+	interval := flag.Int("i", 3000, "An interval(ms) of a watchers' file check loop")
+	flag.Parse()
+
+	bytes, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		fmt.Errorf("An error occurred: %s \n\n", err.Error())
+		os.Exit(-1)
+	}
+
+	config := &Config{}
+	if err := json.Unmarshal(bytes, config); err != nil {
+		fmt.Errorf("An error occurred: %s \n\n", err.Error())
+		os.Exit(-1)
+	}
+
+	jobsC := make(chan Job, len(config.Watchers))
+
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Errorf("An error occurred: %s \n\n", err.Error())
+		os.Exit(-1)
+	}
+
+	for _, watcher := range config.Watchers {
+		go watcher.Launch(wd, *interval, jobsC)
+	}
+	handleJobs(jobsC)
+}
+
+func handleJobs(jobsC <-chan Job) {
+	for job := range jobsC {
+		cmd := exec.Command("touch", job.Path)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		// fmt.Printf("%s %s\n", job.Kind, job.Path)
+		if err := cmd.Start(); err != nil {
+			fmt.Errorf("An error occurred touch: %s \n\n", err.Error())
+			os.Exit(-1)
+		}
+		if job.Kind == "deleted" {
+			rmCmd := exec.Command("rm", job.Path)
+			rmCmd.Stdout = os.Stdout
+			rmCmd.Stderr = os.Stderr
+			if err := rmCmd.Start(); err != nil {
+				fmt.Errorf("An error occurred rm: %s \n\n", err.Error())
+			}
+		}
+	}
+}
+
+func (w *Watcher) Launch(watchDir string, interval int, jobsC chan<- Job) {
+	w.JobsC = jobsC
+	w.Targets = make(map[string]map[string]FileStatus)
+	targetDir := watchDir
+	if w.Directory != "" {
+		r := regexp.MustCompile(`^/`)
+		if r.MatchString(w.Directory) {
+			targetDir = w.Directory
+		} else {
+			targetDir = watchDir + "/" + w.Directory
+		}
+	}
+	w.readDir(targetDir, true)
+	for {
+		time.Sleep(time.Duration(interval) * time.Millisecond)
+		w.readDir(targetDir, false)
+	}
+}
+
+func (w *Watcher) readDir(dirname string, init bool) error {
+	fileInfos, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		return err
+	}
+	for _, fileInfo := range fileInfos {
+		name := fileInfo.Name()
+		modTime := fileInfo.ModTime().UnixNano()
+		switch {
+		case w.exclude(name):
+		case strings.HasPrefix(name, "."):
+		case fileInfo.IsDir():
+			if err := w.readDir(dirname+"/"+name, init); err != nil {
+				return err
+			}
+		default:
+			_, prs := w.Targets[dirname]
+			if !prs {
+				w.Targets[dirname] = make(map[string]FileStatus)
+			}
+			if init {
+				w.Targets[dirname][name] = FileStatus{ModTime: modTime}
+			} else {
+				preservedFileInfo, prs := w.Targets[dirname][name]
+				if !prs {
+					w.Targets[dirname][name] = FileStatus{ModTime: modTime, LastModTime: modTime}
+					w.sendJob(dirname, name, "created")
+				} else if preservedFileInfo.LastModTime != 0 && preservedFileInfo.LastModTime != modTime {
+					w.Targets[dirname][name] = FileStatus{ModTime: modTime, LastModTime: 0}
+				} else if preservedFileInfo.LastModTime == 0 && preservedFileInfo.ModTime != modTime {
+					w.Targets[dirname][name] = FileStatus{ModTime: modTime, LastModTime: modTime}
+					w.sendJob(dirname, name, "updated")
+				}
+			}
+		}
+	}
+	if !init {
+		preservedFileInfos, prs := w.Targets[dirname]
+		if prs {
+			for name, _ := range preservedFileInfos {
+				exist := false
+				for _, fileInfo := range fileInfos {
+					if name == fileInfo.Name() {
+						exist = true
+						break
+					}
+				}
+				if !exist {
+					delete(w.Targets[dirname], name)
+					w.sendJob(dirname, name, "deleted")
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (w *Watcher) sendJob(dirname, name, action string) {
+	w.JobsC <- Job{Kind: action, Path: dirname + "/" + name}
+}
+
+func (w *Watcher) exclude(filename string) bool {
+	for _, excludeFilename := range w.Excludes {
+		if filename == excludeFilename {
+			return true
+		}
+	}
+	return false
+}
